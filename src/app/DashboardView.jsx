797815de29a180289a16afd49e4f00bdc26a0bf9
Jose Antonio <antonio-mora777@hotmail.com>
2025-11-27 12:52:49 -0600

    sasa

--- src/app/DashboardView.jsx
+++ src/app/DashboardView.jsx
@@ -97,7 +97,6 @@ export default function DashboardView({
                                         />
                                     )}
 
-                                    {/* LISTA DE SUSCRIPCIONES */}
                                     <SubscriptionList
                                         subscriptions={filteredSubscriptions}
                                         subscriptionStates={subscriptionStates}
