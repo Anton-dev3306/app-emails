35f672d0c8e5fb4b88bd9357b13a0a6fcda76af4
Jose Antonio <antonio-mora777@hotmail.com>
2025-12-17 15:09:55 -0600

    Cmponente del dashboard principal(donde se unifican todos los componentes y se contruye la SPA)

--- src/app/DashboardView.jsx
+++ src/app/DashboardView.jsx
@@ -20,7 +20,6 @@ export default function DashboardView({
                                           analysisProgress,
                                           finalStats,
                                           subscriptionStates,
-                                          onToggleSubscription,
                                           onToggleSpam,
                   
                                           notification,
