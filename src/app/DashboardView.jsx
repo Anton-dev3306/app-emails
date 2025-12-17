be76ca7377b2ff34bea9a0e6019de3c13e4fa105
Jose Antonio <antonio-mora777@hotmail.com>
2025-12-17 15:10:17 -0600

    Cmponente del dashboard principal(donde se unifican todos los componentes y se contruye la SPA)

--- src/app/DashboardView.jsx
+++ src/app/DashboardView.jsx
@@ -63,6 +63,8 @@ export default function DashboardView({
                                 <AnalyzeButton
                                     isAnalyzing={isAnalyzing}
                                     onAnalyze={onAnalyze}
+                                    analysisProgress={analysisProgress}
+                                    finalStats={finalStats}
                                 />
                             </Flex>
 
