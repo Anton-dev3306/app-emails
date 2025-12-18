2d6c81b6146fffb59667588b3031cb0ec9b3b883
Jose Antonio <antonio-mora777@hotmail.com>
2025-12-18 14:09:52 -0600

    Pagina de presentacion

--- src/app/LandingPage.jsx
+++ src/app/LandingPage.jsx
@@ -9,9 +9,10 @@ export default function LandingPage({ onGetStarted, isLoading }) {
             <Container>
                 <section className="py-10  text-center">
                     <div className="max-w-3xl mx-auto">
-                        <div className="inline-flex items-center justify-center w-20 h-20 mb-8 bg-gradient-to-br from-blue-500 to-indigo-600 rounded-2xl shadow-lg shadow-blue-500">
-                            <Mail className="text-blue-300" size={48} strokeWidth={2} />
-                        </div>
+                        <div className="flex items-center justify-center gap-4 mb-6">
+                            <div className="inline-flex items-center justify-center w-15 h-15 bg-gradient-to-br from-blue-500 to-indigo-600 rounded-2xl shadow-lg shadow-blue-500">
+                                <Mail className="text-blue-300" size={40} strokeWidth={2} />
+                            </div>
 
                         <h1 className="text-5xl md:text-6xl font-bold text-gray-900 mb-6 leading-tight text-balance">
                             Detector de Suscripciones
